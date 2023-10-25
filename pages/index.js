fa1a96ada41c70823a3500622b606e67769e2611
Marius Golabski <mgolabski@gmail.com>
2023-10-25 13:46:13 +0200

    add dummy-data and eventlist component

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,16 @@
-import Image from "next/image";
 import { Inter } from "next/font/google";
+import { getFeaturedEvents } from "@/dummy-data";
+import EventList from "@/components/events/EventList";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function HomePage() {
+  const featuredEvents = getFeaturedEvents();
+
   return (
     <main className={`min-h-screen ${inter.className}`}>
       <h1>Home</h1>
+      <EventList featuredEvents={featuredEvents} />
     </main>
   );
 }
