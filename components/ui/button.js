3e69b48a5c804a8581482d1197d6ab780eae8293
Marius Golabski <mgolabski@gmail.com>
2023-10-28 17:17:41 +0200

    remove unnecessary css class and html-tag

--- components/ui/button.js
+++ components/ui/button.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Button({ children, link }) {
   return (
     <Link href={link}>
-      <div className="inline-block bg-blue-500 text-white font-medium px-4 py-2 rounded-md hover:bg-blue-600 transition duration-300">
+      <div className="inline-block bg-gray-950 hover:bg-gray-800 text-white font-medium px-4 py-2 rounded-md transition duration-300">
         {children}
       </div>
     </Link>
