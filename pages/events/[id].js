fd5297342638aefc673a86be8c5707781fe20fd9
Marius Golabski <mgolabski@gmail.com>
2023-10-28 14:16:28 +0200

    refactor EventDetailPage to display event details

--- pages/events/[id].js
+++ pages/events/[id].js
@@ -1,3 +1,32 @@
+import EventSummary from "@/components/event-detail/EventSummary";
+import EventContent from "@/components/event-detail/EventContent";
+import { getEventById } from "@/dummy-data";
+import { useRouter } from "next/router";
+import EventLogistics from "@/components/event-detail/EventLogistics";
+
 export default function EventDetailPage() {
-  return <h1>Event Detail</h1>;
+  const router = useRouter();
+  const eventId = router.query.id;
+  const event = getEventById(eventId);
+
+  if (!event) {
+    return <p>No event found!</p>;
+  }
+
+  return (
+    <main className="min-h-screen">
+      <article>
+        <EventSummary title={event.title} />
+        <EventLogistics
+          date={event.date}
+          address={event.location}
+          image={event.image}
+          imageAlt={event.title}
+        />
+        <EventContent>
+          <p>{event.description}</p>
+        </EventContent>
+      </article>
+    </main>
+  );
 }
