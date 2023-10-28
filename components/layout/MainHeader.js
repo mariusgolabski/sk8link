4d8e386cd73e06869d16b4a8095d00a6b536348e
Marius Golabski <mgolabski@gmail.com>
2023-10-28 17:15:13 +0200

    add MainHeader component

---
+++ components/layout/MainHeader.js
@@ -0,0 +1,20 @@
+import Link from "next/link";
+import Button from "../ui/button";
+
+export default function MainHeader() {
+  return (
+    <header className="container p-4 mx-auto">
+      <nav className="flex justify-between items-center">
+        <Link className="font-extrabold text-2xl" href="/">
+          sk8link
+        </Link>
+        <ul>
+          <li>
+            <Link href="events">All Events</Link>
+          </li>
+        </ul>
+        <Button link="#">Get Started</Button>
+      </nav>
+    </header>
+  );
+}
