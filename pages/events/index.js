80ee849570fdad9182ba480f656cfd1421c43c8a
Marius Golabski <mgolabski@gmail.com>
2023-11-15 10:41:06 +0100

    refactor code to use EventList and EventsSearch components

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,3 +1,13 @@
+import { getAllEvents } from "@/dummy-data";
+import EventList from "@/components/events/EventList";
+import EventsSearch from "@/components/events/EventsSearch";
+
 export default function AllEventsPage() {
-  return <h1>All Events</h1>;
+  const events = getAllEvents();
+  return (
+    <>
+      <EventsSearch />
+      <EventList events={events} />
+    </>
+  );
 }
