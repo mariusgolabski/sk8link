d52a077fe92e27a2026b26aab0df898da87a6e0a
Marius Golabski <mgolabski@gmail.com>
2023-12-06 13:29:16 +0100

    add notification component to display global notifications

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,10 +1,22 @@
+import { useContext } from "react";
+import Notification from "../ui/Notification";
 import MainHeader from "./MainHeader";
+import NotificationContext from "@/store/NotificationContext";
 
 export default function Layout({ children }) {
+  const notificationCtx = useContext(NotificationContext);
+  const activeNotification = notificationCtx.notification;
   return (
     <>
       <MainHeader />
       <main className="container mx-auto px-4">{children}</main>
+      {activeNotification && (
+        <Notification
+          title={activeNotification.title}
+          message={activeNotification.message}
+          status={activeNotification.status}
+        />
+      )}
     </>
   );
 }
