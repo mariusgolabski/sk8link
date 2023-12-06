50348b913c862ff05d4c93704415f0fd83075a10
Marius Golabski <mgolabski@gmail.com>
2023-12-07 00:12:47 +0100

    refine styling and structure

--- components/events/EventsSearch.js
+++ components/events/EventsSearch.js
@@ -14,34 +14,51 @@ export default function EventsSearch({ onSearch }) {
   }
 
   return (
-    <form onSubmit={submitHandler}>
-      <div>
-        <div>
-          <label htmlFor="year">Year</label>
-          <select id="year" ref={yearInputRef}>
-            <option value="2023">2023</option>
-            <option value="2024">2024</option>
-          </select>
+    <div className="max-w-2xl mx-auto pt-10 pb-16">
+      <form className="flex gap-4 items-end" onSubmit={submitHandler}>
+        <div className="flex-1">
+          <label htmlFor="year">
+            <span className="block text-neutral-800 font-medium text-sm">
+              Year
+            </span>
+            <select
+              className="block w-full border border-neutral-200 bg-white rounded-lg text-sm font-normal h-11 px-4 py-2 mt-1"
+              id="year"
+              ref={yearInputRef}
+            >
+              <option value="2023">2023</option>
+              <option value="2024">2024</option>
+            </select>
+          </label>
         </div>
-        <div>
-          <label htmlFor="month">Month</label>
-          <select id="month" ref={monthInputRef}>
-            <option value="1">January</option>
-            <option value="2">February</option>
-            <option value="3">March</option>
-            <option value="4">April</option>
-            <option value="5">May</option>
-            <option value="6">June</option>
-            <option value="7">July</option>
-            <option value="8">August</option>
-            <option value="9">Septemper</option>
-            <option value="10">October</option>
-            <option value="11">November</option>
-            <option value="12">Dezember</option>
-          </select>
+
+        <div className="flex-1">
+          <label htmlFor="month">
+            <span className="block text-neutral-800 font-medium text-sm">
+              Month
+            </span>
+            <select
+              className="block w-full border border-neutral-200 bg-white rounded-lg text-sm font-normal h-11 px-4 py-2 mt-1"
+              id="month"
+              ref={monthInputRef}
+            >
+              <option value="1">January</option>
+              <option value="2">February</option>
+              <option value="3">March</option>
+              <option value="4">April</option>
+              <option value="5">May</option>
+              <option value="6">June</option>
+              <option value="7">July</option>
+              <option value="8">August</option>
+              <option value="9">September</option>
+              <option value="10">October</option>
+              <option value="11">November</option>
+              <option value="12">December</option>
+            </select>
+          </label>
         </div>
-      </div>
-      <Button>Find Events</Button>
-    </form>
+        <Button className="h-11">Find Events</Button>
+      </form>
+    </div>
   );
 }
