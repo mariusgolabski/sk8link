7a0feb8546aba8fe67452746aca7a202d0e7b97c
Marius Golabski <mgolabski@gmail.com>
2023-12-07 00:07:58 +0100

    delete whitespaces

--- components/ui/button.js
+++ components/ui/button.js
@@ -4,7 +4,7 @@ export default function Button({ children, link, onClick }) {
   if (link) {
     return (
       <Link href={link}>
-        <div className="inline-block bg-gray-950 hover:bg-gray-800 text-white font-medium px-4 py-2 rounded-md transition duration-300">
+        <div className="inline-block bg-gray-950 hover:bg-gray-800 text-white font-medium px-4 py-2 rounded-md transition duration-300 h-11">
           {children}
         </div>
       </Link>
@@ -12,7 +12,7 @@ export default function Button({ children, link, onClick }) {
   }
   return (
     <button
-      className="inline-block bg-gray-950 hover:bg-gray-800 text-white font-medium px-4 py-2 rounded-md transition duration-300"
+      className="inline-block bg-gray-950 hover:bg-gray-800 text-white font-medium px-4 py-2 rounded-md transition duration-300 h-11"
       onClick={onClick}
     >
       {children}
